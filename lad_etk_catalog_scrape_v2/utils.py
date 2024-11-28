50f080c17b201d52a0129a77f875b5205e21d54c
Ikars Melnalksnis <ikars.melnalksnis@llu.lv>
2024-11-28 16:50:39 +0200

    Initial new model matching alg

--- lad_etk_catalog_scrape_v2/utils.py
+++ lad_etk_catalog_scrape_v2/utils.py
@@ -1,4 +1,7 @@
 import os, json, requests, re
+from rapidfuzz import fuzz
+from sentence_transformers import SentenceTransformer, util
+from equipment_model import EquipmentModel
 def open_json(file):
     data = {}
     if os.path.exists(file):
@@ -97,3 +100,32 @@ def first_or_default(data: list, fn):
 # https://www.calculatorsoup.com/calculators/conversions/power.php zs -> kw 
 def convert_zs_to_kw(zs):
     return float(zs) * 0.7457
+
+
+def rapidfuzz_similarity(a: str, b: str) -> float:
+    normalized_a = normalize_text(a)
+    normalized_b = normalize_text(b)
+    return fuzz.token_sort_ratio(normalized_a, normalized_b) / 100
+
+def sentence_transformer_similarity(model: SentenceTransformer, a: str, b: str) -> float:
+    normalized_a = normalize_text(a)
+    normalized_b = normalize_text(b)
+    embedding_a = model.encode(normalized_a)
+    embedding_b = model.encode(normalized_b)
+
+    return util.cos_sim(embedding_a, embedding_b).item()
+
+# fuzzy and sentence matching
+def sentence_transformer_fuzzy_similarity(model: SentenceTransformer, a: str, b: str) -> float: 
+    fuzzy_score = rapidfuzz_similarity(a, b)
+    sentence_similarity = sentence_transformer_similarity(model, a, b)
+    return (fuzzy_score * 0.6) + (sentence_similarity * 0.4)
+
+def get_full_model_name(equipmentModel: EquipmentModel) -> str:
+    return f"{equipmentModel['manufacturer']} {equipmentModel['model']}"
+
+def models_similar(model: SentenceTransformer, equipmentA: EquipmentModel, equipmentB: EquipmentModel, threshold: float = 0.90) -> bool:
+    # Todo: Must use more information to better match, models are not enough
+    similarity = sentence_transformer_fuzzy_similarity(model, get_full_model_name(equipmentA), get_full_model_name(equipmentB))
+
+    return similarity >= threshold
