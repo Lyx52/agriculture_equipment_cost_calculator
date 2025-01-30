45ab9f32de16689a0d4beb28acd015708c52039a
Lyx52 <lyxikars123@gmail.com>
2025-01-30 03:03:34 +0200

    Add mascus

--- build_equipment_catalog/tasks/task_build_mascus_data.py
+++ build_equipment_catalog/tasks/task_build_mascus_data.py
@@ -1,44 +1,24 @@
 from utils import normalize_text, save_json, open_json, convert_zs_to_kw
 import os, re
 
-x = {
-    "productId": "{4529AFB1-3600-48D9-9018-BEA5714F81E8}",
-    "brand": "Amazone",
-    "model": "Cirrus 4003",
-    "yearOfManufacture": 2019,
-    "catalogName": "agriculture",
-    "categoryName": "agricombinationdrills",
-    "meterReadout": null,
-    "meterReadoutUnit": null,
-    "locationCountryCode": "LV",
-    "locationCity": "-",
-    "priceEURO": 33000,
-    "priceOriginal": 33000,
-    "priceOriginalUnit": "EUR",
-    "priceInUserCurrency": 33000,
-    "userCurrency": "EUR",
-    "imageUrl": "https://dnge9sb91helb.cloudfront.net/image/product/medium/4529afb1/amazone-cirrus-4003,1f6d15a2.jpg",
-    "assetUrl": "/lauksaimnieciba/kombinetas-sejmasinas/amazone-cirrus-4003/grevm1nn.html",
-    "companyUrl": "",
-    "companyCountry": "",
-    "imageCount": 10,
-    "videoCount": 0,
-    "companyName": "",
-    "companyId": "",
-    "featured": true,
-    "highlighted": false,
-    "latest24hAd": false,
-    "rentalAd": false,
-    "auctionAd": false,
-    "favoriteAd": false,
-    "createDate": "2025-01-09T08:32:37.44",
-    "sellerPhone": "+371 20135525"
-},
+
+path_to_mascus_data = "../data/mascus"
+mascus_data = open_json(f"{path_to_mascus_data}/mascus.json")
+mascus_categories = open_json(f"{path_to_mascus_data}/mascus_category_map.json")
 
 def build_from_listing(listing):
     return {
         "mark": listing["brand"],
         "model": listing["model"],
         "price": listing["priceEURO"],
-        
+        "mascusCategory": mascus_categories[listing["categoryName"]].replace('-', '_'),
+        "manufactureYear": listing["yearOfManufacture"],
+        "motorHours": listing["meterReadout"] if listing["meterReadoutUnit"] == "h" else None,
+        "drivenDistance": listing["meterReadout"] if listing["meterReadoutUnit"] == "km" else None,
     }
+results = []
+for listing in mascus_data['listings']:
+    results.append(build_from_listing(listing))
+
+print(len(results))
+save_json(f"{path_to_mascus_data}/mascus_short.json", results)
